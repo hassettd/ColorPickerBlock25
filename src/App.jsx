b2c60d1c0176a9d5ed75d2565c66826832a84347
hassettd <hassettd@gmail.com>
2024-11-12 21:37:57 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+            
+      <Button count={count} setCount={setCount} />
+          
+    </div>
+  );
+}
+
+function Button(props) {
+  console.log("Props: ", props);
+  return (
+    <button
+      onClick={() => {
+        props.setCount(props.count+1);
+      }}
+    >
+            {props.count}
+          
+    </button>
+  );
+}
+
+//--- object deconstruction w props ---
+//
+// function Button({count, setCount}) {
+//
+//   return (
+//     <button
+//       onClick={() => {
+//         setCount(count+1)
+//       }}
+//     >
+//       {count}
+//     </button>
+//   );
+// }
